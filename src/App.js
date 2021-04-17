6cd2eee48b0204771bce44e1c4804afd104e0395
Mellang <dlwpgh23@naver.com>
2021-04-17 21:29:28 +0900

    11장 컴포넌트 성능 최적화

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import { useCallback, useState, useRef } from 'react';
 
-function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: '리액트기초 알기', checked: true },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링',
-      checked: true,
-    },
-    { id: 3, text: '일정관리 앱 만들기', checked: false },
-  ]);
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `라노벨 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  // const [todos, setTodos] = useState([
+  //   { id: 1, text: '리액트기초 알기', checked: true },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링',
+  //     checked: true,
+  //   },
+  //   { id: 3, text: '일정관리 앱 만들기', checked: false },
+  // ]);
+
+  const [todos, setTodos] = useState(createBulkTodos);
 
   const nextId = useRef(4);
+  // const nextId = useRef(2501);
 
   const onInsert = useCallback(
     (text) => {
@@ -28,14 +42,16 @@ function App() {
       setTodos(todos.concat(todo));
       nextId.current += 1; //nextId 1씩 더하기
     },
-    [todos],
+    // [todos],
+    [],
   );
 
   const onRemove = useCallback(
     (id) => {
       setTodos(todos.filter((todo) => todo.id !== id));
     },
-    [todos],
+    // [todos],
+    [],
   );
 
   const onToggle = useCallback(
@@ -54,6 +70,6 @@ function App() {
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
-}
+};
 
 export default App;
